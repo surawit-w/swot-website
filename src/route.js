d163c0baf208ef598d2a7b390186379c00aee23d
EarthSurawit <surawitwiriya@kkumail.com>
2020-05-15 04:33:08 +0700

    แก้ไปเรื่อยตอนน้

--- src/route.js
+++ src/route.js
@@ -17,43 +17,36 @@ import preswot from "./js-file/pages/preswotPage";
 import olcPage from "./js-file/pages/olcPage";
 import olcScore from "./js-file/pages/olcScore";
 import howtoPage from "./js-file/pages/howtoPage";
-import preswotolcPage from './js-file/pages/preswotolcPage'
-import swotolcPage from './js-file/pages/swotolcPage'
-import swotolcPage2 from './js-file/pages/swotolcPage2'
-import swotolcPage3 from './js-file/pages/swotolcPage3'
+import preswotolcPage from "./js-file/pages/preswotolcPage";
+import swotolcPage from "./js-file/pages/swotolcPage";
+import swotolcPage2 from "./js-file/pages/swotolcPage2";
+import swotolcPage3 from "./js-file/pages/swotolcPage3";
 // PUBLIC STATIC MAIN CLASS AS ROUTER
 export default class route extends Component {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact path="/" component={mainPage} />
-          <Route exact path="/howto" component={howtoPage} />
-          <Route exact path="/swot-analysis" component={swotPage} />
-          <Route exact path="/swot-analysis2" component={swotPage2} />
-          <Route exact path="/swot-analysis3" component={swotPage3} />
-          <Route exact path="/swot-info" component={swotInfoPage} />
-          <Route exact path="/olc-info" component={olcInfoPage} />
-          <Route exact path="/swot-analysis-weakness" component={Weakness} />
-          <Route
-            exact
-            path="/swot-analysis-opportunity"
-            component={Opportunity}
-          />
-          <Route exact path="/swot-analysis-threat" component={Threat} />
-          <Route exact path="/swot-score" component={scorePage} />
-          <Route exact path="/swot-score2" component={scorePage2} />
-          <Route exact path="/swot-result" component={resultPage} />
-          <Route exact path="/swot-result2" component={resultPage2} />
-          <Route exact path="/pre-swot" component={preswot} />
-          <Route exact path="/olc-analysis" component={olcPage} />
-          <Route exact path="/olc-score" component={olcScore} />
-          <Route exact path="/pre-swotolc" component={preswotolcPage}/>
-          <Route exact path="/swotolc-analysis" component={swotolcPage}/>
-          <Route exact path="/swotolc-analysis2" component={swotolcPage2}/>
-          <Route exact path="/swotolc-analysis3" component={swotolcPage3}/>
-
-        </Switch>
+        <Route exact path="/" component={mainPage} />
+        <Route path="/howto" component={howtoPage} />
+        <Route path="/swot-analysis" component={swotPage} />
+        <Route path="/swot-analysis2" component={swotPage2} />
+        <Route path="/swot-analysis3" component={swotPage3} />
+        <Route path="/swot-info" component={swotInfoPage} />
+        <Route path="/olc-info" component={olcInfoPage} />
+        <Route path="/swot-analysis-weakness" component={Weakness} />
+        <Route path="/swot-analysis-opportunity" component={Opportunity} />
+        <Route path="/swot-analysis-threat" component={Threat} />
+        <Route path="/swot-score" component={scorePage} />
+        <Route path="/swot-score2" component={scorePage2} />
+        <Route path="/swot-result" component={resultPage} />
+        <Route path="/swot-result2" component={resultPage2} />
+        <Route path="/pre-swot" component={preswot} />
+        <Route path="/olc-analysis" component={olcPage} />
+        <Route path="/olc-score" component={olcScore} />
+        <Route path="/pre-swotolc" component={preswotolcPage} />
+        <Route path="/swotolc-analysis" component={swotolcPage} />
+        <Route path="/swotolc-analysis2" component={swotolcPage2} />
+        <Route path="/swotolc-analysis3" component={swotolcPage3} />
       </Router>
     );
   }
